d1a9e0ad0afaf72bc594951d3ca9f6503de6f023
Pedro Rodrigues <peddroprp@gmail.com>
2020-10-24 10:21:30 -0300

    Resolve bugs com middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import * as dotenv from 'dotenv';
 import * as jsonServer from 'json-server';
 import * as routes from './routes.json';
-import middlewares from './middlewares/generate-data';
-import genData from './middlewares/generate-data';
+// import middlewares from './middlewares/generate-data';
+// import genData from './middlewares/generate-data';
 
 dotenv.config();
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
 const port = 3000;
 
-genData();
+// genData();
 server.use(jsonServer.bodyParser);
 server.use(jsonServer.rewriter(routes));
-server.use(middlewares);
+// server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
   console.log(`Mock direct participant is running on port ${port}`);
