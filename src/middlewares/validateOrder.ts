129f4be52de248cde071d721c1c4667a609dd0d7
Mateus Roriz <mateusvroriz6b@gmail.com>
2020-11-04 00:33:41 -0300

    adicionando validação para carteira e order_ref

--- src/middlewares/validateOrder.ts
+++ src/middlewares/validateOrder.ts
@@ -1,5 +1,6 @@
 const cpfValidation = keystring => {
-  const cpf = /^[0-9]{11}$/;
+  // const cpf = /^[0-9]{11}$/; cpf sem - ou .
+  const cpf = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
   return cpf.test(keystring);
 };
 
@@ -19,27 +20,46 @@ const surnameValidation = keystring => {
 };
 
 const phoneValidation = keystring => {
-  const phone = /^\+[1-9][0-9]\d{1,14}$/;
+  const phone = /^(1[ \-\+]{0,3}|\+1[ -\+]{0,3}|\+1|\+)?((\(\+?1-[2-9][0-9]{1,2}\))|(\(\+?[2-8][0-9][0-9]\))|(\(\+?[1-9][0-9]\))|(\(\+?[17]\))|(\([2-9][2-9]\))|([ \-\.]{0,3}[0-9]{2,4}))?([ \-\.][0-9])?([ \-\.]{0,3}[0-9]{2,4}){2,3}$/;
+  // const phone = /^\+[1-9][0-9]\d{1,14}$/; numero de telefone +55 
   return phone.test(keystring);
 };
 
+const itemValidation = keystring => {
+  const item_title = /^[a-z ,.'-]+$/i;
+  return item_title.test(keystring);
+};
+
+const walletValidation = keystring => {
+  const wallet =  /(?:pix|mercadopago|picpay|ame|pagseguro|cielo)/;
+  return wallet.test(keystring);
+};
+
+const orderefValidation = keystring => {
+   const order_ref =  /^.{1,255}$/;
+
+   if (typeof(keystring) == "string" && order_ref.test(keystring) == true){
+      return order_ref.test(keystring);
+   }
+   return false;
+};
+
+const itemsListValidation = keystring => {
+  const item_title = /^[a-z ,.'-]+$/i;
+  return item_title.test(keystring);
+};
+
 const middlewareValidateDict = (req, res, next) => {
-  let keystring = req.query.Key;
-
-  console.log('validando dados pedido');
-
-  if (req.method === 'POST' && keystring) {
-    // a query string está trocando o + por " " na requisição
-    // o replace desfaz essa alteração
-    keystring = keystring.replace(' ', '+');
-    req.query.Key = keystring;
-
+  if (req.method === 'POST') {
     if (
-      cpfValidation(keystring) ||
-      phoneValidation(keystring) ||
-      emailValidation(keystring) ||
-      nameValidation(keystring) ||
-      surnameValidation(keystring)
+      cpfValidation(req.body.buyer.cpf) &&
+      emailValidation(req.body.buyer.email) &&
+      nameValidation(req.body.buyer.first_name) &&
+      surnameValidation(req.body.buyer.last_name) &&
+      phoneValidation(req.body.buyer.phone) &&
+      orderefValidation(req.body.order_ref) &&
+      walletValidation(req.body.wallet) 
+      
     ) {
       next();
     } else {
